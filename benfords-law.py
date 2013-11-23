69fc5d2b89ec1f3aa6f18157102f2c68215ceb28
Krzysztof Jański <krzysztof.janski_at+g^m!a[i!l{.}c]o!m>
2013-11-23 23:38:32 +0100

    digits renamed to digits_histogram

--- benfords-law.py
+++ benfords-law.py
@@ -2,29 +2,29 @@
 
 import unittest
 
-def format_result(digits, passed_benfords_law):
-	return str(digits) + "\n" + passed_benfords_law + "\n"
+def format_result(digits_histogram, passed_benfords_law):
+	return str(digits_histogram) + "\n" + passed_benfords_law + "\n"
 
 class benfords_law:
 	def __init__(self, text):
 		words = text.split()
-		self.digits = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+		self.digits_histogram = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 		for word in words:
 			if word[0].isdigit():
-				self.digits[int(word[0])] += 1
+				self.digits_histogram[int(word[0])] += 1
 
 	def get_result(self):
-		return format_result(self.digits, "not passed")
+		return format_result(self.digits_histogram, "not passed")
 
 class test_benfords_law(unittest.TestCase):
 	def test_one_number(self):
 		text = "12345"
-		digits = {1:1, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text).get_result(), format_result(digits, "not passed"))
+		digits_histogram = {1:1, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+		self.assertEqual(benfords_law(text).get_result(), format_result(digits_histogram, "not passed"))
 
 	def test_two_numbers_separated_by_space(self):
 		text = "12345 234"
-		digits = {1:1, 2:1, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text).get_result(), format_result(digits, "not passed"))
+		digits_histogram = {1:1, 2:1, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+		self.assertEqual(benfords_law(text).get_result(), format_result(digits_histogram, "not passed"))
 
 unittest.main()
