3e21515f5ab4d215aa58eb2223a5600350cec91d
Krzysztof Jański <krzysztof.janski_at+g^m!a[i!l{.}c]o!m>
2013-11-23 22:49:35 +0100

    function benfords_law changed into a class

--- benfords-law.py
+++ benfords-law.py
@@ -5,23 +5,26 @@ import unittest
 def format_result(digits, whether_passed):
 	return str(digits) + "\n" + whether_passed + "\n"
 
-def benfords_law(text):
-	words = text.split()
-	digits = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-	for word in words:
-		if word[0].isdigit():
-			digits[int(word[0])] += 1
-	return format_result(digits, "not passed")
+class benfords_law:
+	def __init__(self, text):
+		words = text.split()
+		self.digits = {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
+		for word in words:
+			if word[0].isdigit():
+				self.digits[int(word[0])] += 1
+
+	def get_result(self):
+		return format_result(self.digits, "not passed")
 
 class test_benfords_law(unittest.TestCase):
 	def test_one_number(self):
 		text = "12345"
 		digits = {1:1, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text), format_result(digits, "not passed"))
+		self.assertEqual(benfords_law(text).get_result(), format_result(digits, "not passed"))
 
 	def test_two_numbers_separated_by_space(self):
 		text = "12345 234"
 		digits = {1:1, 2:1, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text), format_result(digits, "not passed"))
+		self.assertEqual(benfords_law(text).get_result(), format_result(digits, "not passed"))
 
 unittest.main()
