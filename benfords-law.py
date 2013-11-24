297df810d6a1ff17085ee4fed3422fa32f7037be
Krzysztof Jański <krzysztof.janski_at+g^m!a[i!l{.}c]o!m>
2013-11-24 20:20:15 +0100

    result class and it's formatting

--- benfords-law.py
+++ benfords-law.py
@@ -3,14 +3,25 @@
 import unittest
 import math
 
-def format_result(digits_histogram, digits_frequencies, has_passed_benfords_law):
-	return str(digits_histogram) + "\n" + str(digits_frequencies) + "\n" + has_passed_benfords_law + "\n"
+class result:
+	def __init__(self, histogram, frequancies, passed):
+		self.histogram = histogram
+		self.frequencies = frequancies
+		self.passed = passed
+
+def format_result(result):
+	output = ""
+	bfd = benfords_frequncy_distribution()
+	for digit in range(1, 10):
+		output += str(digit) + ":\t" + str(result.histogram[digit]) + "\t"
+		output += str(result.frequencies[digit]) + "\t" + str(bfd[digit]) + "\t" + str(result.passed[digit]) + "\n"
+	return output
 
 def digits():
 	return {1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 
 def benfords_frequncy_distribution():
-	return {x: math.log(1 + (1/x), 10)}
+	return {x: math.log(1 + (1/x), 10) for x in range (1, 10)}
 
 class benfords_law:
 
@@ -38,19 +49,36 @@ class benfords_law:
 		return digits_frequencies
 
 	def get_result(self):
-		return format_result(self.digits_histogram, self.calculate_frequencies(), "not passed")
+		return format_result(result(self.digits_histogram, self.calculate_frequencies(), {x: False for x in range (1, 10)}))
 
 class test_benfords_law(unittest.TestCase):
 	def test_one_number(self):
 		text = "12345"
 		digits_histogram = {1:1, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 		digits_frequencies = {1:100, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text).get_result(), format_result(digits_histogram, digits_frequencies, "not passed"))
+		digits_passed = {x: False for x in range (1, 10)}
+		r = result(digits_histogram, digits_frequencies, digits_passed)
+		self.assertEqual(benfords_law(text).get_result(), format_result(r))
 
 	def test_two_numbers_separated_by_space(self):
 		text = "12345 234"
 		digits_histogram = {1:1, 2:1, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
 		digits_frequencies = {1:50, 2:50, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0, 9:0}
-		self.assertEqual(benfords_law(text).get_result(), format_result(digits_histogram, digits_frequencies, "not passed"))
+		digits_passed = {x: False for x in range (1, 10)}
+		r = result(digits_histogram, digits_frequencies, digits_passed)
+		self.assertEqual(benfords_law(text).get_result(), format_result(r))
+
+class test_format_result(unittest.TestCase):
+	def test_print(self):
+		digits_histogram = {x: x for x in range(1, 10)}
+		digits_frequencies = {x: 10 - x for x in range(1, 10)}
+		digits_passed = {x: (x % 2) == 0 for x in range(1, 10)}
+		r = result(digits_histogram, digits_frequencies, digits_passed)
+		bfd = benfords_frequncy_distribution()
+		output = ""
+		for digit in range(1, 10):
+			output += str(digit) + ":\t" + str(digits_histogram[digit]) + "\t"
+			output += str(digits_frequencies[digit]) + "\t" + str(bfd[digit]) + "\t" + str(digits_passed[digit]) + "\n"
+		self.assertEqual(output, format_result(r))
 
 unittest.main()
