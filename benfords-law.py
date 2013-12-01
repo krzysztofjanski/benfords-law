7a2d38f21b7cab62ec105626f95d53908ff9a79c
Krzysztof Jański <krzysztof.janski_at+g^m!a[i!l{.}c]o!m>
2013-12-01 17:33:36 +0100

    treshold option

--- benfords-law.py
+++ benfords-law.py
@@ -6,8 +6,9 @@ import copy
 import blm
 
 parser = argparse.ArgumentParser(description="Check a text document against Benford's law.")
-
+parser.add_argument('-t', '--treshold', dest='treshold', type=float, default=0,
+	help='treshold to match frequencies')
 args = parser.parse_args()
 
-bl = blm.benfords_law(sys.stdin.read(), 0.01)
+bl = blm.benfords_law(sys.stdin.read(), args.treshold)
 print bl.get_result()
